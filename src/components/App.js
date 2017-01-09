41ce793c92620c258ca59213cdcf2cc2508240bc
xwang1024 <xwang1024@126.com>
2017-01-09 09:41:18 +0800

    add ant-design

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React, {Component, PropTypes} from 'react'
+import { DatePicker, message } from 'antd';
 
 class componentName extends Component {
   constructor (props) {
     super(props)
 
+  }
+
+  state = {
+    date: ''
   }
 
   componentWillMount () {
@@ -30,17 +35,19 @@ class componentName extends Component {
 
   }
 
+  handleChange(date) {
+      message.info('您选择的日期是: ' + date.toString());
+      this.setState({ date });
+  }
+
   render () {
     return (
       <div>
-        <h1>Hello React!</h1>
+        <DatePicker onChange={value => this.handleChange(value)} />
+        <div style={{ marginTop: 20 }}>当前日期：{this.state.date.toString()}</div>
       </div>
     )
   }
-}
-
-componentName.propTypes = {
-
 }
 
 export default componentName
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import {render} from 'react-dom';
 
 import App from './components/App';
 import './resources/index.less';
+import 'antd/dist/antd.css';
 
 (function run() {
   render(
